66379393d1565504bbccfb111dddf967be82ca7d
Javier Lona <javierlona1@gmail.com>
2018-08-06 13:50:12 -0500

    added the win case scenario

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num');
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guesses
+guessBtn.addEventListener('click', function(){
+  // Convert string to integer
+  let guess = parseInt(guessInput.value);
+  console.log(guess);
+
+  // Validate
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if won
+  if(guess === winningNum){
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = 'green';
+    // Set message
+    setMessage(`${winningNum} is correct!, YOU WIN`, 'green');
+  } else {
+    
+  }
+});
+
+// Set message
+function setMessage($msg, $color){
+  message.style.color = $color;
+  message.textContent = $msg;
+}
